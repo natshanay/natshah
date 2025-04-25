3a50c29da17ee7bcc970c17a34ab4d9502a96e72
natnael <natnaelchala10@gmail.com>
2025-04-25 05:16:43 -0700

    cdcd

---
+++ src/app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Header from "./components/header";
+import { ClerkProvider,ClerkLoading,ClerkLoaded} from "@clerk/nextjs";
+import Loader from "./components/Loader";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "NExt auth",
+  description: "next auth with clerk and mongodb",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ClerkLoading>
+        <Loader/>            
+          </ClerkLoading>
+          <ClerkLoaded>
+
+        <Header/>
+        {children}
+          </ClerkLoaded>
+      </body>
+    </html>
+        </ClerkProvider>
+
+  );
+}
