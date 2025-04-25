6f1b3502aba7f0a8c9c9f5eba0a8eb6acedd45fe
natnael <natnaelchala10@gmail.com>
2025-04-25 08:28:43 -0700

    cdcdcdccdcdd

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -10,7 +10,9 @@ export async function POST(req: NextRequest) {
     const eventType = evt.type
     console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
     console.log('Webhook payload:', evt.data)
-
+    if (evt.type === 'user.created') {
+        console.log('userId:', evt.data.id)
+      }
     return new Response('Webhook received', { status: 200 })
   } catch (err) {
     console.error('Error verifying webhook:', err)
